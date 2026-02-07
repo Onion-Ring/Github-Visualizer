ead36616767945bf121efc7ab75ad898826bab15
OnionRing <antoniocg285@gmail.com>
2026-02-07 12:19:32 +0100

    Publishing changes on Github Pages at 02/07/2026 12:19:31

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { UserSearch } from "./user-search/user-search";
 import { UserInfo } from "./user-info/user-info";
 import { Header } from "./header/header";
+import { SocialNetworks } from "./social-networks/social-networks";
 
 @Component({
   selector: 'app-root',
-  imports: [UserSearch, UserInfo,Header],
+  imports: [UserSearch, UserInfo, Header, SocialNetworks],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
